93bc8f65bdef3af68d0a50f22503b11ddba61da6
Alaina Thompson <athompson9@hawk.iit.edu>
2022-01-25 10:44:47 -0600

    Initial Commit

---
+++ Shared/Homework_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Homework_1App.swift
+//  Shared
+//
+//  Created by Alaina Thompson on 1/25/22.
+//
+
+import SwiftUI
+
+@main
+struct Homework_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
